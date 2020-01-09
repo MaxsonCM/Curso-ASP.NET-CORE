1aaa4e291311271dc887ca3f49b884b852b95a3b
Maxson C. Maidlinger <max_maidlinger@hotmail.com>
2020-01-09 09:43:36 -0300

    View Components
    Exemplo de como se implementa as views components

---
+++ FaixaEtariaViewComponent/FaixaEtariaViewComponent/ViewComponents/AdolecenteViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using FaixaEtariaViewComponent.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FaixaEtariaViewComponent.ViewComponents
+{
+    public class AdolecenteViewComponent : ViewComponent
+    {
+        private readonly PessoaContexto _pessoaContexto;
+
+        public AdolecenteViewComponent(PessoaContexto pessoaContexto)
+        {
+            _pessoaContexto = pessoaContexto;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _pessoaContexto.Pessoas.Where( x => x.Idade < 18).ToListAsync());
+        }
+    }
+}
