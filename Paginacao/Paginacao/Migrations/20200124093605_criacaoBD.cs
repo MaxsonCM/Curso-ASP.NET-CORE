afa64ab061a3dbf556a966d4b2dee20f3c3e0e6b
Maxson C. Maidlinger <max_maidlinger@hotmail.com>
2020-01-24 07:05:01 -0300

    Paginação
    usando o X.PagedList para dividir as lista de consultas

---
+++ Paginacao/Paginacao/Migrations/20200124093605_criacaoBD.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Paginacao.Migrations
+{
+    public partial class criacaoBD : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pessoas",
+                columns: table => new
+                {
+                    PessoaId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    idade = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoas", x => x.PessoaId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pessoas");
+        }
+    }
+}
