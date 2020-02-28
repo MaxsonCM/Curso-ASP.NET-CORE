85d697db948baa482258a0741e56754316222164
Maxson C. Maidlinger <max_maidlinger@hotmail.com>
2020-02-28 07:28:00 -0300

    usando Cookies
    usando Cookies

--- Cookies/Cookies/Controllers/HomeController.cs
+++ Cookies/Cookies/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Cookies.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace Cookies.Controllers
 {
@@ -26,7 +27,34 @@ namespace Cookies.Controllers
         {
             ViewData["Message"] = "Your contact page.";
 
+            try {
+                var visitante = Request.Cookies["Visitante"].ToString();
+
+                if (visitante.Equals("Sim"))
+                    return View("Visitante");
+
+            } catch (Exception e){}
+
             return View();
+            }
+
+        public IActionResult EscreverCookies()
+        {
+            CookieOptions cookie = new CookieOptions();
+            cookie.Expires = DateTime.Now.AddHours(1);
+            cookie.HttpOnly = true;
+            cookie.Secure = true;
+
+            Response.Cookies.Append("Visitante", "Sim", cookie);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult ExcluirCookies()
+        {            
+            Response.Cookies.Delete("Visitante");
+
+            return RedirectToAction("Index");
         }
 
         public IActionResult Privacy()
