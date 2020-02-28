a1ab8f7596ae11147947f5e316c16ef9583f3f2d
Maxson C. Maidlinger <max_maidlinger@hotmail.com>
2020-02-27 21:09:37 -0300

    Gerando PDF
    Criando arquivos do tipo pdf com o nuget Rotativa.AspNetCore

---
+++ arquivoPDF/arquivoPDF/Models/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace arquivoPDF.Models
+{
+    public class Pessoa
+    {
+        public int PessoaId { get; set; }
+
+        public String Nome { get; set; }
+
+        public int Idade { get; set; }
+
+    }
+}
