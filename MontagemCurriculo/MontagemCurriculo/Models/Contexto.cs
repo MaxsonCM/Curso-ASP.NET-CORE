ac532c827bab367dde69e6005cae737bcf75ae5f
Maxson C. Maidlinger <max_maidlinger@hotmail.com>
2020-03-01 11:08:55 -0300

    Classes
    Criandos as classes das entidades.
    Configurando as notações dos campos.

--- MontagemCurriculo/MontagemCurriculo/Models/Contexto.cs
+++ MontagemCurriculo/MontagemCurriculo/Models/Contexto.cs
@@ -8,6 +8,15 @@ namespace MontagemCurriculo.Models
 {
     public class Contexto : DbContext
     {
+
+        public DbSet<Curriculo> Curriculos { get; set; }
+        public DbSet<Idioma> Idiomas { get; set; }
+        public DbSet<TipoCurso> TiposCursos { get; set; }
+        public DbSet<FormacaoAcademica> FormacoesAcademicas { get; set; }
+        public DbSet<Objetivo> Objetivos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+
         public Contexto(DbContextOptions<Contexto> opcoes): base(opcoes)
         {
 
---
+++ MontagemCurriculo/MontagemCurriculo/Models/Usuario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MontagemCurriculo.Models
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+
+        [Required(ErrorMessage ="Campo obrigatório")]
+        [StringLength(50,ErrorMessage ="Use menos caracteres")]
+        [EmailAddress(ErrorMessage ="E-mail inválido")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [StringLength(50, ErrorMessage = "Use menos caracteres")]        
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        public ICollection<InformacaoLogin> InformacoesLogin { get; set; }
+
+        public ICollection<Curriculo> Curriculos { get; set; }
+
+    }
+}
